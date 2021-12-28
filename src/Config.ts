ed9aad27f574ba602decac9bbec1b07214ec71b5
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-28 21:11:42 +0000

    Added configuration settings

---
+++ src/Config.ts
@@ -0,0 +1,130 @@
+'use strict';
+import { workspace } from 'vscode';
+
+export interface IProxy {
+    enable: boolean;
+    baseUri: string;
+    proxyUri: string;
+}
+
+export interface IHttps {
+    enable: boolean;
+    cert: string;
+    key: string;
+    passphrase: string;
+}
+
+export class Config {
+
+    public static get configuration() {
+        return workspace.getConfiguration('liveServer.settings');
+    }
+
+    private static getSettings<T>(val: string): T {
+        return Config.configuration.get(val) as T;
+    }
+
+    private static setSettings(key: string, val: number, isGlobal: boolean = false): Thenable<void> {
+        return Config.configuration.update(key, val, isGlobal);
+    }
+
+    public static get getHost(): string {
+        return Config.getSettings<string>('host');
+    }
+
+    public static get getLocalIp(): string {
+        return Config.getSettings<string>('useLocalIp');
+    }
+
+    public static get getPort(): number {
+        return Config.getSettings<number>('port');
+    }
+
+    public static setPort(port: number): Thenable<void> {
+        return Config.setSettings('port', port);
+    }
+
+    public static get getRoot(): string {
+        return Config.getSettings<string>('root');
+    }
+
+    public static get getNoBrowser(): boolean {
+        return Config.getSettings<boolean>('NoBrowser');
+    }
+
+    public static get getUseBrowserPreview(): boolean {
+        return Config.getSettings<boolean>('useBrowserPreview');
+    }
+
+    public static get getAdvancedBrowserCmdline(): string {
+        return Config.getSettings<string>('AdvanceCustomBrowserCmdLine');
+    }
+
+    public static get getChromeDebuggingAttachment(): boolean {
+        return Config.getSettings<boolean>('ChromeDebuggingAttachment');
+    }
+
+    public static get getCustomBrowser(): string {
+        return Config.getSettings<string>('CustomBrowser');
+    }
+
+    public static get getIgnoreFiles(): string[] {
+        return Config.getSettings<string[]>('ignoreFiles');
+    }
+
+    public static get getDonotShowInfoMsg(): boolean {
+        return Config.getSettings<boolean>('donotShowInfoMsg');
+    }
+
+    public static setDonotShowInfoMsg(val: boolean, isGlobal: boolean = false) {
+        Config.configuration.update('donotShowInfoMsg', val, isGlobal);
+    }
+
+    public static get getDonotVerifyTags(): boolean {
+        return Config.getSettings<boolean>('donotVerifyTags');
+    }
+
+    public static setDonotVerifyTags(val: boolean, isGlobal: boolean = false) {
+        Config.configuration.update('donotVerifyTags', val, isGlobal);
+    }
+
+    public static get getUseWebExt(): boolean {
+        return Config.getSettings<boolean>('useWebExt') || false;
+    }
+
+    public static get getProxy(): IProxy {
+        return Config.getSettings<IProxy>('proxy');
+    }
+
+    public static get getHttps(): IHttps {
+        return Config.getSettings<IHttps>('https') || {} as IHttps;
+    }
+
+    public static get getWait(): number {
+        return Config.getSettings<number>('wait');
+    }
+
+    public static get getfullReload(): boolean {
+        return Config.getSettings<boolean>('fullReload');
+    }
+
+    public static get getMount(): Array<Array<string>> {
+        return Config.getSettings<Array<Array<string>>>('mount');
+    }
+
+    public static get getShowOnStatusbar(): boolean {
+        return Config.getSettings<boolean>('showOnStatusbar') || false;
+    }
+
+    public static get getFile(): string {
+        return Config.getSettings<string>('file');
+    }
+
+    public static get getMutiRootWorkspaceName(): string {
+        return Config.getSettings<string>('multiRootWorkspaceName');
+    }
+
+    public static setMutiRootWorkspaceName(val: string) {
+       return Config.configuration.update('multiRootWorkspaceName', val, false);
+    }
+}
---
+++ src/Helper.ts
@@ -0,0 +1,155 @@
+'use strict';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Config } from './Config';
+
+export const SUPPRORTED_EXT: string[] = [
+    '.html', '.htm', '.svg'
+];
+
+export const isRelativePath = (pathUrl: string) => {
+    if (pathUrl.startsWith('*')) return false;
+
+    return !path.isAbsolute(pathUrl);
+};
+
+
+export class Helper {
+
+
+    public static testPathWithRoot(workSpacePath: string) {
+
+        let rootPath: string;
+
+        // Test the path is actually exists or not
+        const testPath = path.join(workSpacePath, Config.getRoot);
+
+        let isNotOkay = !fs.existsSync(testPath);
+        if (!isNotOkay) { // means okay :)
+            rootPath = testPath;
+        }
+        else {
+            rootPath = workSpacePath;
+        }
+
+        if (!rootPath.endsWith(path.sep))
+            rootPath = rootPath + path.sep;
+
+        return {
+            isNotOkay,
+            rootPath
+        };
+    }
+
+    /**
+     * This function return the remaining path from root to target.
+     * e.g. : root is `c:\user\rootfolder\` and target is `c:\user\rootfolder\subfolder\index.html`
+     * then this function will return `subfolder\index.html` as html is a supported otherwise it will return null.
+     *
+     * @param rootPath
+     * @param targetPath
+     */
+    public static getSubPath(rootPath: string, targetPath: string) {
+
+        if (!Helper.IsSupportedFile(targetPath) || !targetPath.startsWith(rootPath)) {
+            return null;
+        }
+
+        return targetPath.substring(rootPath.length, targetPath.length);
+    }
+
+    /**
+     * It returns true if file is supported. input can be in full file path or just filename with extension name.
+     * @param file: can be path/subpath/file.ts or file.ts
+     */
+    public static IsSupportedFile(file: string): boolean {
+        let ext = path.extname(file) || (file.startsWith('.') ? file : `.${file}`);
+        return SUPPRORTED_EXT.indexOf(ext.toLowerCase()) > -1;
+    }
+
+
+    /**
+     *
+     * @param rootPath
+     * @param workspacePath
+     * @param onTagMissedCallback
+     */
+    public static generateParams(
+        rootPath: string,
+        workspacePath: string,
+        onTagMissedCallback?: MethodDecorator
+    ) {
+
+        workspacePath = workspacePath || '';
+        const port = Config.getPort;
+        const ignorePathGlob = Config.getIgnoreFiles || [];
+
+        const ignoreFiles = [];
+        ignorePathGlob.forEach(ignoredPath => {
+            if (isRelativePath(ignoredPath))
+                ignoreFiles.push(path.join(workspacePath, ignoredPath));
+            else ignoreFiles.push(ignoredPath);
+        });
+
+        const proxy = Helper.getProxySetup();
+        const https = Helper.getHttpsSetup();
+
+        const mount = Config.getMount;
+        // In live-server mountPath is reslove by `path.resolve(process.cwd(), mountRule[1])`.
+        // but in vscode `process.cwd()` is the vscode extensions path.
+        // The correct path should be resolve by workspacePath.
+        mount.forEach((mountRule: Array<any>) => {
+            if (mountRule.length === 2 && mountRule[1]) {
+                mountRule[1] = path.resolve(workspacePath, mountRule[1]);
+            }
+        });
+
+        const file = Config.getFile;
+        return {
+            port: port,
+            host: '0.0.0.0',
+            root: rootPath,
+            file: file,
+            open: false,
+            https: https,
+            ignore: ignoreFiles,
+            disableGlobbing: true,
+            proxy: proxy,
+            cors: true,
+            wait: Config.getWait || 100,
+            fullReload: Config.getfullReload,
+            useBrowserExtension: Config.getUseWebExt,
+            onTagMissedCallback: onTagMissedCallback,
+            mount: mount
+        };
+    }
+
+    static getHttpsSetup() {
+        const httpsConfig = Config.getHttps;
+        let https = null;
+        if (httpsConfig.enable === true) {
+            let cert = fs.readFileSync(httpsConfig.cert, 'utf8');
+            let key = fs.readFileSync(httpsConfig.key, 'utf8');
+            https = {
+                cert: cert,
+                key: key,
+                passphrase: httpsConfig.passphrase
+            };
+        }
+
+        return https;
+    }
+
+    static getProxySetup() {
+        const proxySetup = Config.getProxy;
+        let proxy = [[]];
+        if (proxySetup.enable === true) {
+            proxy[0].push(proxySetup.baseUri, proxySetup.proxyUri);
+        }
+        else {
+            proxy = null; // required to change the type [[]] to black array [].
+        }
+
+        return proxy;
+    }
+}
