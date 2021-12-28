5fc9adb7c819dc498246bca7f5c02f79f46f54dc
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-28 21:26:58 +0000

    Finished the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,29 @@
-"use strict";
-import { ExtensionContext, workspace, commands, window } from "vscode";
-import { AppModel } from "./appModel";
-import { checkNewAnnouncement } from "./announcments";
+'use strict';
+import { ExtensionContext, workspace, commands, window } from 'vscode';
+import { AppModel } from './appModel';
+import { checkNewAnnouncement } from './announcement';
+
+export function activate(context: ExtensionContext) {
+    const appModel = new AppModel();
+
+    checkNewAnnouncement(context.globalState);
+    context.subscriptions.push(commands
+        .registerCommand('extension.liveServer.goOnline', async (fileUri) => {
+            await workspace.saveAll();
+            appModel.Golive(fileUri ? fileUri.fsPath : null);
+        })
+    );
+
+    context.subscriptions.push(commands
+        .registerCommand('extension.liveServer.goOffline', () => {
+            appModel.GoOffline();
+        })
+    );
+
+    context.subscriptions.push(commands
+        .registerCommand('extension.liveServer.changeWorkspace', () => {
+            appModel.changeWorkspaceRoot();
+        })
+    );
+    context.subscriptions.push(appModel);
+}
